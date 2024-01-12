68dc850bf49c6e523214870fe53737e30f079f6f
Mudassar Ali <mudassar.ali@vortx.com>
2024-01-13 03:41:47 +0500

    Add project files.

---
+++ OperatorsandExpressions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_OperatorsandExpressions
+{
+    public class OperatorsandExpressions
+    {
+        public int a { get; set; } = 10;
+        public int b { get; set; } = 20;
+
+        //Arithmetic Operators
+
+        //Addation Operation
+        public int AddArthmaticOperation()
+        {
+            return a + b;
+        }
+
+        //Subtraction Operation
+        public int SubArthmaticOperation()
+        { 
+            return a - b;
+        }
+
+        //Mutiplation Operation
+        public int MultiplicationArthmaticOperation()
+        {
+            return b * a;
+        }
+
+        //Division Operation
+        public int DivisionOperation()
+        {
+            return a / b;
+        }
+
+        //Mod Operation
+        public int ModOperation() { return a % b;}
+
+    }
+}
